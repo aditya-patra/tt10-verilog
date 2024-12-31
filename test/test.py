52bf8ac48ff1106a761b3ea14cbdb2f57d95f223
aditya-patra <64704485+aditya-patra@users.noreply.github.com>
2024-12-31 11:46:20 -0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -20,9 +20,8 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 2)
+    await ClockCycles(dut.clk, 15)
     dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 2)
 
     dut._log.info("Test project behavior")
 
@@ -35,29 +34,33 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.ui_in.value = 0b00000000  # Sensor 1 off
     await ClockCycles(dut.clk, 10)
-
+    assert dut.uo_out == 0b00000001
     # Test case 3: Enable ui_in[1] (sensor2)
     dut.ui_in.value = 0b00000010  # Sensor 2 on
     await ClockCycles(dut.clk, 10)
     dut.ui_in.value = 0b00000000  # Sensor 2 off
     await ClockCycles(dut.clk, 10)
+    assert dut.uo_out == 0b00000010
 
     # Test case 4: Enable ui_in[2] (sensor3)
     dut.ui_in.value = 0b00000100  # Sensor 3 on
     await ClockCycles(dut.clk, 10)
     dut.ui_in.value = 0b00000000  # Sensor 3 off
     await ClockCycles(dut.clk, 10)
+    assert dut.uo_out == 0b00000100
 
     # Test case 5: Combination of ui_in[1] and ui_in[2]
     dut.ui_in.value = 0b00000110  # Sensor 2 and 3 on
-    await ClockCycles(dut.clk, 40)
+    await ClockCycles(dut.clk, 10)
     dut.ui_in.value = 0b00000000  # All sensors off
     await ClockCycles(dut.clk, 10)
+    assert dut.uo_out == 0b00000010
 
     # Test case 6: All sensors enabled
     dut.ui_in.value = 0b11111111  # All sensors on
-    await ClockCycles(dut.clk, 20)
+    await ClockCycles(dut.clk, 10)
     dut.ui_in.value = 0b00000000  # All sensors off
     await ClockCycles(dut.clk, 10)
+    assert dut.uo_out == 0b00000001
 
     dut._log.info("End of test")
