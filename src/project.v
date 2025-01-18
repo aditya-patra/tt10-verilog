a15a74fee229453eb97225061315e1a7b02a0e0a
aditya-patra <64704485+aditya-patra@users.noreply.github.com>
2025-01-18 10:09:43 -0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2,8 +2,8 @@
 // 3 input state enable signals from ui_in[0:2]
 // 3 output state signals to uo_out[0:2]
 
-// project is meant to warn visually impaired user of obstacles around them by using 3 LIDAR distance sensors as input and connecting speakers to state output
-// when LIDAR sensor sends logic 1(object is close), state machine enables corresponding speaker warning user of obstacle in the direction of the LIDAR sensor
+// project is meant to warn visually impaired user of obstacles around them by using 3 distance sensors as input and connecting alerting devices to state output
+// when distance sensor sends logic 1(object is close), state machine enables corresponding alerting device warning user of obstacle in the direction of the sensor
 
 module tt_um_aditya_patra(
     input wire [7:0] ui_in,      // Using ui_in [2:0]
@@ -21,14 +21,14 @@ module tt_um_aditya_patra(
     reg [1:0] next_state;
 
     
-    reg speaker1;  // output signal of state 1
-    reg speaker2;  // output signal of state 2
-    reg speaker3;  // output signal of state 3
+    reg warning1;  // output signal of state 1
+    reg warning2;  // output signal of state 2
+    reg warning3;  // output signal of state 3
     
     // connecting state output signals to uo_out
-    assign uo_out[0] = speaker1;
-    assign uo_out[1] = speaker2;
-    assign uo_out[2] = speaker3;
+    assign uo_out[0] = warning1;
+    assign uo_out[1] = warning2;
+    assign uo_out[2] = warning3;
 
     // assigning default value to unused output signals
     assign uo_out[3] = 1'b0;
@@ -58,41 +58,41 @@ module tt_um_aditya_patra(
     // Sequential logic for state and counter updates
     always @(*) begin
         if (ena) begin
-            //check current state and update output speakers accordingly
+            //check current state and update output warnings accordingly
             case (curr_state)
-                //no speakers are logic 1
+                //no warnings are logic 1
                 STATE_0:begin
-                    speaker1 <= 0;
-                    speaker2 <= 0;
-                    speaker3 <= 0;
+                    warning1 <= 0;
+                    warning2 <= 0;
+                    warning3 <= 0;
                 end
-                //speaker1 is logic 1
+                //warning1 is logic 1
                 STATE_1:begin
-                    speaker1 <= 1;
-                    speaker2 <= 0;
-                    speaker3 <= 0;
+                    warning1 <= 1;
+                    warning2 <= 0;
+                    warning3 <= 0;
                 end
-                //speaker2 is logic 1
+                //warning2 is logic 1
                 STATE_2:begin
-                    speaker1 <= 0;
-                    speaker2 <= 1;
-                    speaker3 <= 0;
+                    warning1 <= 0;
+                    warning2 <= 1;
+                    warning3 <= 0;
                 end
-                //speaker3 is logic 1
+                //warning3 is logic 1
                 STATE_3:begin
-                    speaker1 <= 0;
-                    speaker2 <= 0;
-                    speaker3 <= 1;
+                    warning1 <= 0;
+                    warning2 <= 0;
+                    warning3 <= 1;
                 end
-                //all speakers logic 0 if curr_state has unknown value
+                //all warning2 logic 0 if curr_state has unknown value
                 default:begin
-                    speaker1 <= 0;
-                    speaker2 <= 0;
-                    speaker3 <= 0;
+                    warning1 <= 0;
+                    warning2 <= 0;
+                    warning3 <= 0;
                 end
             endcase
                 
-            //check which speaker is logic 1 and update next_state accordingly
+            //check which sensor is logic 1 and update next_state accordingly
             //if sensor1 is logic 1, change curr_state to 1
             if (sensor1 == 1) begin
                 next_state <= STATE_1;
